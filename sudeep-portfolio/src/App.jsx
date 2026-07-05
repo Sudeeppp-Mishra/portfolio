b8705ab83792f39402c6b9a66f9dcca23157360d
Sudeep Mishra <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2026-07-05 13:59:43 +0545

    Add App.jsx shell composing layout and Home page

--- sudeep-portfolio/src/App.jsx
+++ sudeep-portfolio/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Navbar from './components/layout/Navbar.jsx';
+import ScrollProgress from './components/layout/ScrollProgress.jsx';
+import BackToTop from './components/layout/BackToTop.jsx';
+import Footer from './components/layout/Footer.jsx';
+import Home from './pages/Home.jsx';
 
-const App = () => {
+function App() {
   return (
-    <div>Hello</div>
-  )
+    <>
+      <ScrollProgress />
+      <Navbar />
+      <main>
+        <Home />
+      </main>
+      <Footer />
+      <BackToTop />
+    </>
+  );
 }
 
-export default App
+export default App;
